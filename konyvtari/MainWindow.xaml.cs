df0ee2f6b83ac1129f2355b1716a39306094ba11
kormonditamas2222 <t.kormond2006@gmail.com>
2025-09-24 11:49:48 +0200

    4.óra végi állapot

--- konyvtari/MainWindow.xaml.cs
+++ konyvtari/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,90 @@ namespace konyvtari
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> nevek = [];
+        
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();  
+            lb_nevek.ItemsSource = nevek;
+            Megnyitas();
+        }
+
+        private void btn_be_Click(object sender, RoutedEventArgs e)
+        {
+            string nev = "";
+            if (string.IsNullOrEmpty(tbox_username.Text.ToString()))
+            {
+                nev = "Nem lett megadva név";
+            }
+            else
+            {
+                nev = tbox_username.Text.ToString();
+            }
+            int kor;
+            bool szamE = int.TryParse(tbox_age.Text, out kor);
+            if (szamE)
+            {
+                kor = int.Parse(tbox_age.Text);
+            }
+            else
+            {
+                MessageBox.Show(this, "A életkorhoz számot kell megadni!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            string ertesitesek = "";
+            if (cb_hirlevel.IsChecked == true)
+            {
+                ertesitesek += "Hírlevél ";
+            }
+            if (cb_sms.IsChecked == true) 
+            { 
+                ertesitesek += "SMS"; 
+            }
+            if (cb_ujsag.IsChecked == true) 
+            {
+                ertesitesek += "Újság"; 
+            }
+            else
+            {
+                ertesitesek = "Nem kér";
+            }
+            string tagsag = "";
+            if (rb_diak.IsChecked == true)
+            {
+                tagsag = "diak";
+            }
+            else if (rb_felnott.IsChecked == true)
+            {
+                tagsag = "felnőtt";
+            }
+            else if (rb_nyugdijas.IsChecked == true)
+            {
+                tagsag = "nyugdíjas";
+            }
+            else
+            {
+                tagsag = "Nincs kijelölve";
+            }
+            txt_confirm.Text = "Sikeres mentés";
+            Olvaso olvaso = new Olvaso(nev, kor, ertesitesek, tagsag);
+            Fajlkeszites(olvaso);
+        }
+        private void Fajlkeszites(Olvaso olvaso)
+        {
+            string filePath = @"S:\Petrik\12.E\csharp\konyvtari\konyvtari\olvasok.txt";
+            string content = olvaso.ToString() ?? "";
+            File.AppendAllText(filePath, content);
+        }
+        private void Megnyitas()
+        {
+            string filePath = @"S:\Petrik\12.E\csharp\konyvtari\konyvtari\olvasok.txt";
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] reszek = line.Trim().Split(", ");
+                nevek.Add(reszek[0]);
+            }
+            lb_nevek.Items.Refresh();
         }
     }
 }
---
+++ konyvtari/Olvaso.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvtari
+{
+    class Olvaso
+    {
+        string nev;
+        int kor;
+        string ertesites;
+        string tagsag;
+
+        public Olvaso(string nev, int kor, string ertesites, string tagsag)
+        {
+            this.nev = nev;
+            this.kor = kor;
+            this.ertesites = ertesites;
+            this.tagsag = tagsag;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Kor { get => kor; set => kor = value; }
+        public string Ertesites { get => ertesites; set => ertesites = value; }
+        public string Tagsag { get => tagsag; set => tagsag = value; }
+
+        public override string? ToString()
+        {
+            return $"{nev}, {kor}, {ertesites}, {tagsag}";
+        }
+    }
+}
